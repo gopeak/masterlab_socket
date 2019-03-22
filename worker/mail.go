53272004d21a77385e8603c07444cd7adaa8ba71
sven <121642038@qq.com>
2019-03-22 19:31:25 +0800

    重构和优化了邮件发送函数

--- worker/mail.go
+++ worker/mail.go
@@ -3,7 +3,7 @@ package worker
 import (
 	"fmt"
 	_ "fmt"
-	"github.com/antonholmquist/jason"
+	"github.com/json-iterator/go"
 	"gopkg.in/gomail.v2"
 	"masterlab_socket/lib"
 	"masterlab_socket/util"
@@ -16,6 +16,44 @@ import (
 type loginAuth struct {
 	username, password string
 }
+
+type MailContent struct {
+	Seq         string   `json:"seq"`
+	Host        string   `json:"host"`
+	Port        string   `json:"port"`
+	User        string   `json:"user"`
+	Password    string   `json:"password"`
+	From        string   `json:"from"`
+	FromName    string   `json:"from_name"`
+	To          []string `json:"to"`
+	Cc          []string `json:"cc"`
+	Bcc         []string `json:"bcc"`
+	ContentType string   `json:"content_type"`
+	Subject     string   `json:"subject"`
+	Body        string   `json:"body"`
+	Attach      string   `json:"attach"`
+}
+
+func (this *MailContent) Init( ) *MailContent {
+
+	this.Seq = ""
+	this.Host = ""
+	this.Port = ""
+	this.User = ""
+	this.Password = ""
+	this.From = ""
+	this.FromName = "Masterlab"
+	this.To = []string{}
+	this.Cc = []string{}
+	this.Bcc = []string{}
+	this.ContentType = "html"
+	this.Subject = ""
+	this.Body = ""
+	this.Attach = ""
+
+	return this
+}
+
 func LoginAuth(username, password string) smtp.Auth {
 	return &loginAuth{username, password}
 }
@@ -41,158 +79,104 @@ func MergeSlice(s1 []string, s2 []string) []string {
 	return slice
 }
 
-func SendToMail(user, password, host, subject, body, mailtype, replyToAddress string, to, cc, bcc []string) error {
-	// hp := strings.Split(host, ":")
-	//auth := smtp.PlainAuth("", user, password, hp[0])
-	auth := LoginAuth(user, password)
-	var content_type string
-	if mailtype == "html" {
-		content_type = "Content-Type: text/" + mailtype + "; charset=UTF-8"
-	} else {
-		content_type = "Content-Type: text/plain" + "; charset=UTF-8"
-	}
+// 通过非加密方式发送
+func sendByNoSSL(mailContent *MailContent) error {
+	replyToAddress := ""
+	auth := LoginAuth(mailContent.User, mailContent.Password)
+	content_type := "Content-Type: text/" + mailContent.ContentType + "; charset=UTF-8"
+	noSslHost := fmt.Sprintf("%s:%s", mailContent.Host, mailContent.Port)
+	cc_address := strings.Join(mailContent.Cc, ";")
+	bcc_address := strings.Join(mailContent.Bcc, ";")
+	to_address := strings.Join(mailContent.To, ";")
+	msg := []byte("To: " + to_address + "\r\nFrom: Masterlab<" + mailContent.User + ">\r\nSubject: " + mailContent.Subject + "\r\nReply-To: " + replyToAddress + "\r\nCc: " + cc_address + "\r\nBcc: " + bcc_address + "\r\n" + content_type + "\r\n\r\n" + mailContent.Body)
 
-	cc_address := strings.Join(cc, ";")
-	bcc_address := strings.Join(bcc, ";")
-	to_address := strings.Join(to, ";")
-	msg := []byte("To: " + to_address + "\r\nFrom: Masterlab<" + user + ">\r\nSubject: " + subject + "\r\nReply-To: " + replyToAddress + "\r\nCc: " + cc_address + "\r\nBcc: " + bcc_address + "\r\n" + content_type + "\r\n\r\n" + body)
-
-	send_to := MergeSlice(to, cc)
-	send_to = MergeSlice(send_to, bcc)
-	err := smtp.SendMail(host, auth, user, send_to, msg)
+	send_to := MergeSlice(mailContent.To, mailContent.Cc)
+	send_to = MergeSlice(send_to, mailContent.Bcc)
+	err := smtp.SendMail(noSslHost, auth, mailContent.User, send_to, msg)
 	return err
 }
+
+// 通过gomail库SSL加密发送
+func  sendBySSL(mailContent *MailContent) error {
+	m := gomail.NewMessage()
+	m.SetHeader("From", mailContent.FromName+"<"+mailContent.From+">", )
+	m.SetHeader("To", mailContent.To...)
+	m.SetHeader("Subject", mailContent.Subject)
+	if mailContent.ContentType == "html" {
+		m.SetBody("text/html", mailContent.Body)
+	} else {
+		m.SetBody("text/plain", mailContent.Body)
+	}
+	if mailContent.Attach != "" && util.Exists(mailContent.Attach) {
+		m.Attach(mailContent.Attach)
+	}
+	port_int, err := strconv.Atoi(mailContent.Port)
+	if err != nil {
+		return err
+	}
+	d := gomail.NewDialer(mailContent.Host, port_int, mailContent.User, mailContent.Password)
+	// Send the email toArr Bob, Cora and Dan.
+	if err := d.DialAndSend(m); err != nil {
+		return err
+	}
+	return nil
+}
+
 func (this TaskType) Mail() ReturnType {
 
 	//sdk:=new(Sdk).Init(this.Cmd,this.Sid,this.Reqid,this.Data )
 	// 获取数据
-	fmt.Println("Mail this.Data:", string(this.Data))
-	json_obj, err := jason.NewObjectFromBytes(this.Data)
-	if err != nil {
-		ret := ReturnType{"failed", "failed", this.ReqHeader.Sid, "json err:" + err.Error()}
-		return ret
-	}
-	host, err := json_obj.GetString("host")
-	if err != nil {
-		ret := ReturnType{"failed", "failed", this.ReqHeader.Sid, "json err:host not found"}
-		return ret
-	}
-	port, err := json_obj.GetString("port")
-	if err != nil {
-		ret := ReturnType{"failed", "failed", this.ReqHeader.Sid, "json err:port not found"}
-		return ret
-	}
-	user, err := json_obj.GetString("user")
-	if err != nil {
-		ret := ReturnType{"failed", "failed", this.ReqHeader.Sid, "json err:user not found"}
-		return ret
-	}
-	password, err := json_obj.GetString("password")
-	if err != nil {
-		ret := ReturnType{"failed", "failed", this.ReqHeader.Sid, "json err:password not found"}
-		return ret
-	}
-	from, err := json_obj.GetString("from")
-	if err != nil {
-		ret := ReturnType{"failed", "failed", this.ReqHeader.Sid, "json err:from not found"}
-		return ret
-	}
-	toArr, err := json_obj.GetStringArray("to")
-	if err != nil {
-		ret := ReturnType{"failed", "failed", this.ReqHeader.Sid, "json err:toArr not found"}
-		return ret
-	}
-	cc, err := json_obj.GetStringArray("cc")
-	if err != nil {
-		cc = nil
-	}
-	bcc, err := json_obj.GetStringArray("bcc")
-	if err != nil {
-		bcc = nil
-	}
-	subject, err := json_obj.GetString("subject")
-	if err != nil {
-		ret := ReturnType{"failed", "failed", this.ReqHeader.Sid, "json err:subject not found"}
-		return ret
-	}
-	body, err := json_obj.GetString("body")
-	if err != nil {
-		ret := ReturnType{"failed", "failed", this.ReqHeader.Sid, "json err:body not found"}
-		return ret
-	}
+	//fmt.Println("Mail this.Data:", string(this.Data))
 
-	fmt.Println(host, port, user, password, from, toArr, subject, body)
-	m := gomail.NewMessage()
-	m.SetHeader("From", from, "Masterlab")
-	toStr := ""
-	for _, to := range toArr {
-		//m.SetHeader("To", to)
-		m.SetAddressHeader("To", to, to)
-		toStr =   fmt.Sprintf("%s;%s", toStr, to)
-	}
-
-	m.SetHeader("Subject", subject)
-	m.SetBody("text/html", body)
-	//m.Attach("D:/timg.jpg")
-	var attach string
-	attach, err = json_obj.GetString("attach")
-	if err != nil {
-		attach = ""
-	}
-	if attach!="" && util.Exists(attach){
-		m.Attach(attach)
-	}
-	port_int, err := strconv.Atoi(port)
-	if err != nil {
-		ret := ReturnType{"filed", "mail", this.ReqHeader.Sid, "port error"}
+	mailContent := new(MailContent).Init()
+	var json = jsoniter.ConfigCompatibleWithStandardLibrary
+	json_err := json.Unmarshal(this.Data, &mailContent)
+	if json_err != nil {
+		ret := ReturnType{"failed", "failed", this.ReqHeader.Sid, "json err:" + json_err.Error()}
 		return ret
 	}
+	fmt.Println(mailContent)
 
+	to_address := strings.Join(mailContent.To, ";")
 	db := new(lib.Mysql)
-	_, err = db.ShortConnect()
+	_, err := db.ShortConnect()
 	if err != nil {
-		ret := ReturnType{ "failed","failed" ,this.ReqHeader.Sid, "数据库连接失败:" + err.Error() }
+		ret := ReturnType{"failed", "failed", this.ReqHeader.Sid, "数据库连接失败:" + err.Error()}
 		return ret
 	}
 	// 获取时间戳
 	now := time.Now().Unix()
-	timestamp := strconv.FormatInt(now,10)
+	timestamp := strconv.FormatInt(now, 10)
 	create_time_nano := fmt.Sprintf("%v", time.Now().UnixNano());
-
-	var seq string
-	seq, err = json_obj.GetString("seq")
-	if err != nil {
-		seq = create_time_nano
+	if mailContent.Seq == "" {
+		mailContent.Seq = create_time_nano
 	}
-	if port=="465" || port=="995"{
-		d := gomail.NewDialer(host, port_int, user, password)
+	if mailContent.Port == "465" || mailContent.Port == "995" {
 		// Send the email toArr Bob, Cora and Dan.
-		if err := d.DialAndSend(m); err != nil {
+		if err := sendBySSL( mailContent ); err != nil {
+			fmt.Println("Send mail error!", err.Error())
 			ret := ReturnType{"filed", "mail", this.ReqHeader.Sid, err.Error()}
-			_, err  = db.Insert("REPLACE INTO   `main_mail_queue` (seq, `title`, `address`, `status`, `create_time`, `error`) VALUES ( ?,?,?,?,?,?)",
-				seq, subject, toStr, "error",timestamp, err.Error())
+			_, err = db.Insert("REPLACE INTO   `main_mail_queue` (seq, `title`, `address`, `status`, `create_time`, `error`) VALUES ( ?,?,?,?,?,?)",
+				mailContent.Seq, mailContent.Subject, to_address, "error", timestamp, err.Error())
 			return ret
 		}
-	}else{
-		m.Reset();
-		noSslHost := fmt.Sprintf("%s:%s", host, port)
-		err := SendToMail(user, password, noSslHost, subject, body, "html", "", toArr, cc, bcc)
+	} else {
+		err := sendByNoSSL(mailContent)
 		if err != nil {
-			fmt.Println("Send mail error!")
+			fmt.Println("Send mail error!", err.Error())
 			ret := ReturnType{"filed", "mail", this.ReqHeader.Sid, err.Error()}
-			_, err  = db.Insert("REPLACE INTO   `main_mail_queue` (seq, `title`, `address`, `status`, `create_time`, `error`) VALUES ( ?,?,?,?,?,?)",
-				seq, subject, toStr, "error",timestamp, err.Error())
+			_, err = db.Insert("REPLACE INTO   `main_mail_queue` (seq, `title`, `address`, `status`, `create_time`, `error`) VALUES ( ?,?,?,?,?,?)",
+				mailContent.Seq, mailContent.Subject, to_address, "error", timestamp, err.Error())
 			return ret
 		}
 	}
 
-	_, err  = db.Insert("REPLACE INTO   `main_mail_queue` (seq, `title`, `address`, `status`, `create_time`, `error`) VALUES ( ?,?,?,?,?,?)",
-		seq, subject, toStr, "done",timestamp, "")
+	_, err = db.Insert("REPLACE INTO   `main_mail_queue` (seq, `title`, `address`, `status`, `create_time`, `error`) VALUES ( ?,?,?,?,?,?)",
+		mailContent.Seq, mailContent.Subject, to_address, "done", timestamp, "")
 	if err != nil {
 		ret := ReturnType{"failed", "mail", this.ReqHeader.Sid, "sql replace into error"}
 		return ret
 	}
-
 	ret := ReturnType{"ok", "mail", this.ReqHeader.Sid, ""}
 	return ret
 }
